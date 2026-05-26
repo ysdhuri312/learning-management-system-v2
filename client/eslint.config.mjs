3020a73a020844551ce0fcce4c958f2ffd458ce3
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-05-26 10:56:34 +0530

    Create next project"

---
+++ client/eslint.config.mjs
@@ -0,0 +1,25 @@
+/** @format */
+
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  {
+    rules: {
+      'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'warn',
+    },
+  },
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
+]);
+
+export default eslintConfig;
