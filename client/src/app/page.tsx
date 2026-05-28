b4ccde7df5fa81fcfd579fe74e688c766aa5b21e
Yogesh Dhuri <ysdhuri312@gmail.com>
2026-05-28 11:38:49 +0530

    Complete Testimonials section

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -2,12 +2,14 @@
 
 import Companies from '@/components/home/Companies';
 import Hero from '@/components/home/Hero';
+import Testimonials from '@/components/home/Testimonials';
 
 export default function Home() {
   return (
     <main className='flex flex-col items-center space-y-7 text-center'>
       <Hero />
       <Companies />
+      <Testimonials />
     </main>
   );
 }
